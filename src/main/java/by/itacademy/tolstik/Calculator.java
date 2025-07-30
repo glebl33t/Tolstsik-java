4a8ce5d6e5d9cd804c3e851537f75377c8734d25
Gleb Tolstik <glebtolst1k@mail.ru>
2025-07-30 21:42:22 +0300

    add Calculator and ICalculator

--- src/main/java/by/itacademy/tolstik/Calculator.java
+++ src/main/java/by/itacademy/tolstik/Calculator.java
@@ -1,4 +1,24 @@
 package by.itacademy.tolstik;
 
-public class Calculator {
+public class Calculator implements ICalculator {
+
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
 }

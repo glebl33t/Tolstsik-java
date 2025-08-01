29876c74091eee0b870551c9a9ab408964d83cf7
Gleb Tolstik <glebtolst1k@mail.ru>
2025-08-01 09:38:18 +0300

    add CalculatorTest

--- src/main/java/by/itacademy/tolstik/Calculator.java
+++ src/main/java/by/itacademy/tolstik/Calculator.java
@@ -4,21 +4,36 @@ public class Calculator implements ICalculator {
 
     @Override
     public int sum(int a, int b) {
-        return a + b;
+        try {
+            return Math.addExact(a, b);
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
     public int subtract(int a, int b) {
-        return a - b;
+        try {
+            return Math.subtractExact(a, b);
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
     public int multiply(int a, int b) {
-        return a * b;
+        try {
+            return Math.multiplyExact(a, b);
+        } catch (Exception e) {
+        }
+        return 0;
     }
 
     @Override
     public double divide(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
         return (double) a / b;
     }
 }
--- src/test/java/by/itacademy/tolstik/CalculatorTest.java
+++ src/test/java/by/itacademy/tolstik/CalculatorTest.java
@@ -1,4 +1,44 @@
 package by.itacademy.tolstik;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void createCalculatorInstance() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("check func sum a + b")
+    public void testSum() {
+        int actual = calculator.sum(2, 5);
+        Assertions.assertEquals(7, actual);
+    }
+
+    @Test
+    @DisplayName("check func subtract a - b")
+    public void testSubtract() {
+        int actual = calculator.subtract(5, 2);
+        Assertions.assertEquals(3, actual);
+    }
+
+    @Test
+    @DisplayName("check func multiply a * b")
+    public void testMultiply() {
+        int actual = calculator.multiply(2, 5);
+        Assertions.assertEquals(10, actual);
+    }
+
+    @Test
+    @DisplayName("check func divide a / b")
+    public void testDivide() {
+        double actual = calculator.divide(10, 5);
+        Assertions.assertEquals(2, actual);
+    }
+
 }

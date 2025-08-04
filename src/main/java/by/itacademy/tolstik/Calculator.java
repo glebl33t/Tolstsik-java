e864440d76730354f99e479ee623e160b880ee87
Gleb Tolstik <glebtolst1k@mail.ru>
2025-08-04 11:42:10 +0300

    refactor code

--- src/main/java/by/itacademy/tolstik/Calculator.java
+++ src/main/java/by/itacademy/tolstik/Calculator.java
@@ -1,39 +1,53 @@
 package by.itacademy.tolstik;
 
+import java.util.function.BiFunction;
+
 public class Calculator implements ICalculator {
 
     @Override
     public int sum(int a, int b) {
-        try {
-            return Math.addExact(a, b);
-        } catch (Exception e) {
-            return 0;
-        }
+        BiFunction<Integer, Integer, Integer> sumFunc = (x, y) -> {
+            try {
+                return Math.addExact(x, y);
+            } catch (ArithmeticException e) {
+                return 0;
+            }
+        };
+        return sumFunc.apply(a, b);
     }
 
     @Override
     public int subtract(int a, int b) {
-        try {
-            return Math.subtractExact(a, b);
-        } catch (Exception e) {
-            return 0;
-        }
+        BiFunction<Integer, Integer, Integer> subtractFunc = (x, y) -> {
+            try {
+                return Math.subtractExact(a, b);
+            } catch (Exception e) {
+                return 0;
+            }
+        };
+        return subtractFunc.apply(a, b);
     }
 
     @Override
     public int multiply(int a, int b) {
-        try {
-            return Math.multiplyExact(a, b);
-        } catch (Exception e) {
-        }
-        return 0;
+        BiFunction<Integer, Integer, Integer> multiplyFunc = (x, y) -> {
+            try {
+                return Math.multiplyExact(a, b);
+            } catch (Exception e) {
+            }
+            return 0;
+        };
+        return multiplyFunc.apply(a, b);
     }
 
     @Override
     public double divide(int a, int b) {
-        if (b == 0) {
-            return 0;
-        }
-        return (double) a / b;
+        BiFunction<Integer, Integer, Double> divideFunc = (x, y) -> {
+            if (b == 0) {
+                return (double) 0;
+            }
+            return (double) a / b;
+        };
+        return divideFunc.apply(a, b);
     }
 }

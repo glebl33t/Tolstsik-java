2456dae64bf7fc98bc1b17826e996ef201afa1e4
Gleb Tolstik <glebtolst1k@mail.ru>
2025-08-07 19:07:28 +0300

    refactor code

--- src/main/java/by/pizzatempo/LoginPageLocator.java
+++ src/main/java/by/pizzatempo/LoginPageLocator.java
@@ -1,10 +1,10 @@
 package by.pizzatempo;
 
 public class LoginPageLocator {
-    public static final String FORM_PERSONAL_ACCOUNT = "//form/div[@class='title']";
+    public static final String TITLE_PERSONAL_ACCOUNT = "//form/div[@class='title']";
     public static final String INPUT_LOGIN = "//input[@name='astroauth_login']";
     public static final String INPUT_PASS = "//input[@name='astroauth_pass']";
     public static final String INPUT_SUBMIT = "//input[@name='astroauth_submit']";
-    public static final String HREF_REGISTRATION = "//a[@href='https://www.pizzatempo.by/registration/']";
-    public static final String HREF_REGISTRATION_FORGOT = "//a[@href='https://www.pizzatempo.by/registration/forgot/']";
+    public static final String BUTTON_REGISTRATION = "//a[@href='https://www.pizzatempo.by/registration/']";
+    public static final String BUTTON_REGISTRATION_FORGOT = "//a[@href='https://www.pizzatempo.by/registration/forgot/']";
 }

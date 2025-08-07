c548fb7b4aa6ee86dc1dfe40030741ad55c87aa5
Gleb Tolstik <glebtolst1k@mail.ru>
2025-08-07 19:04:37 +0300

    add xPath for dodopizza, piazzatempo

---
+++ src/main/java/by/dodopizza/LoginPageLocator.java
@@ -0,0 +1,9 @@
+package by.dodopizza;
+
+public class LoginPageLocator {
+    public static final String BUTTON_PHONE_TITLE = "//div/h3";
+    public static final String SPAN_LABEL_LEFT = "//span[@class='label left']";
+    public static final String INPUT_CHECKOUT_PHONE = "//input[@data-testid='checkout-form__phone-input']";
+    public static final String BUTTON_LOGIN_SUBMIT = "//button[@data-testid='login_submit_button']";
+    public static final String BUTTON_POPUP_CLOSE = "//button[@class='popup-close-button']";
+}
---
+++ src/main/java/by/pizzatempo/LoginPageLocator.java
@@ -0,0 +1,10 @@
+package by.pizzatempo;
+
+public class LoginPageLocator {
+    public static final String FORM_PERSONAL_ACCOUNT = "//form/div[@class='title']";
+    public static final String INPUT_LOGIN = "//input[@name='astroauth_login']";
+    public static final String INPUT_PASS = "//input[@name='astroauth_pass']";
+    public static final String INPUT_SUBMIT = "//input[@name='astroauth_submit']";
+    public static final String HREF_REGISTRATION = "//a[@href='https://www.pizzatempo.by/registration/']";
+    public static final String HREF_REGISTRATION_FORGOT = "//a[@href='https://www.pizzatempo.by/registration/forgot/']";
+}
